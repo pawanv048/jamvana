b9519de1b3ed879a497341fd3a908332dbf14ce9
pawanv048@gmail.com <pawanv048@gmail.com>
2022-07-22 11:47:24 +0530

    drawer

--- App.js
+++ App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { DetailsDataProvider } from './app/context/useDetailsData';
 import Routes from './app/Navigation/Routes';
-import Drawer from './app/Screens/Drawer';
-
 
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
         {/* <Drawer/> */}
       <DetailsDataProvider>
-        <Drawer />
+        <Routes />
       </DetailsDataProvider>
     </SafeAreaView>
   )
